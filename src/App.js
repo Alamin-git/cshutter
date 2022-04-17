e1b10289be1dfa065dba13943db1ca7f0a94dc25
Alamin-git <alamin2.mas@gmail.com>
2022-04-18 04:28:29 +0600

    require auth add done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Blogs from './Pages/Blogs/Blogs';
 import Footer from './Pages/Shared/Footer/Footer';
 import Checkout from './Pages/Checkout/Checkout';
 import Login from './Pages/Login/Login';
+import RequireAuth from './RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -17,7 +18,11 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blog' element={<Blogs></Blogs>}></Route>
-        <Route path='/checkout' element={<Checkout />}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <Checkout />
+          </RequireAuth>}>
+        </Route>
         <Route path='/about' element={<AboutMe></AboutMe>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+   const emailRef = useRef('');
+   const passwordRef = useRef('');
+
+   const handelSubmit = e => {
+      e.preventDefault();
+      console.log(emailRef.current.value, passwordRef.current.value);
+   }
    return (
       <div className='container'>
          <div className="login-form">
             <h3 className='text-center mt-5 mb-3 fs-2 text-secondary fw-bold'>Login</h3>
-            <Form className='w-50 mx-auto  border p-5 rounded'>
+            <Form onSubmit={handelSubmit} className='w-50 mx-auto  border p-5 rounded'>
                <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                   <Form.Text className="text-muted">
                      We'll never share your email with anyone else.
                   </Form.Text>
@@ -18,7 +25,7 @@ const Login = () => {
 
                <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                </Form.Group>
                <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Text>
